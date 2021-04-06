ddd2693d741c06f66a82054436017d104866bff7
i.q <64720410+klanrali@users.noreply.github.com>
2021-04-06 23:55:17 +0300

    Update save.py

--- plugins/save.py
+++ plugins/save.py
@@ -20,10 +20,10 @@ async def saf(e):
     x = await e.get_reply_message()
     if not x:
         return await eod(
-            e, "Reply to Any Message to save it to ur saved messages", time=5
+            e, "Reply to Any Message to save it to ur saved messages - @iqthon", time=5
         )
     await ultroid_bot.send_message("me", x)
-    await eod(e, "Message saved at saved messages", time=5)
+    await eod(e, "Message saved at saved messages - @iqthon", time=5)
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
